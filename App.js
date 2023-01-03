97f0f6e463b2eeabccf0183baa34e8549fc3a451
Krystof Woldrich <krystofwoldrich@gmail.com>
2023-01-03 11:14:37 +0100

    Add sentry-expo

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import * as Sentry from 'sentry-expo';
+
+Sentry.init({
+  dsn: 'YOUR_DSN',
+  tracesSampleRate: 1.0,
+  sampleRate: 1.0,
+  debug: true,
+  enableInExpoDevelopment: true,
+});
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button title='Capture crash' onPress={() => Sentry.Native.captureException(new Error('Error from Expo bare workflow'))} ></Button>
     </View>
   );
 }
